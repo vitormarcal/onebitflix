b447ac6c7aa3c8cb0a84f965f65ce6e457b5d95a
Vítor Marçal <git@vitormarcal.com.br>
2018-05-26 09:52:41 -0300

    gerando os controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :api do
+    namespace :v1 do
+      get '/dashboard', to 'dashboards#index', as: 'dashboard'
+      resources :favorites, path: "my_list", only: %i( index create)
+      delete '/my_list/:type/:id', to: 'favorites#destroy'
+      resources :reviews, only: [:index, :create]
+      resources :searches, path: "search", only: :index
+      resources :series, only: :show
+      resources :movies, only: show do
+        member do
+          get '/executions', to: 'executions#show'
+          get '/executions', to: 'executions#update'
+        end
+      end
+      resources :recomemendations, only: index
+    end
+  end
   root to: "home#index"
 end
