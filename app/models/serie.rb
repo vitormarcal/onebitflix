6160f862b2ad645587e6edf42c74cea34b315cae
Vítor Marçal <vitormarcal8@gmail.com>
2018-05-26 09:17:12 -0300

    validação das models

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -1,3 +1,8 @@
 class Serie < ApplicationRecord
+  include PgSearch
+  multisearchable against: [:title]
   belongs_to :category
+  has_many :reviews, as: :reviewable
+  has_many :episodes, ->{ order(:episode_number) }, class_name: "Movie", dependent: :destroy
+  belongs_to :last_watched_episode, class_name: "Movie", optional: true
 end
