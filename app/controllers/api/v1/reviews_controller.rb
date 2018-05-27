88438b6f2e60bdc721221adaf69ffe36037fa369
Vítor Marçal <vitormarcal8@gmail.com>
2018-05-26 23:33:24 -0300

    refinando os controllers

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::ReviewsController < ApplicationController
+  # skip_before_action :verify_authenticity_token
   def index
+    @reviews = Review.where(reviewable_id: params[:id], reviewable_type: params[:type].capitalize!)
+    render json: Api::V1::ReviewSerializer.new(@reviews, include: [:user]).serialized_json
   end
 
   def create
+    @review = Review.new(review_params)
+    if (@review.save)
+      render json: @review
+    else
+      render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+    end
   end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:reviewable_type, :reviewable_id, :rating, :description).merge(user: current_user)
+    end
 end
